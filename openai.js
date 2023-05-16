973a3641ad99e4c531ecbc9360c013a9716ce1cc
Alberto Barrago <a.barrago69@gmail.com>
2023-05-16 11:51:18 +0200

    Removed file

--- openai.js
+++
@@ -1,6 +0,0 @@
-import {Configuration, OpenAIApi} from 'openai';
-
-const configuration = new Configuration({
-    apiKey: "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",
-});
-export const openai = new OpenAIApi(configuration);
