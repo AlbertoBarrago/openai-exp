aa953077f55c9e118d1c27042a6f152b48b02318
Alberto Barrago <a.barrago69@gmail.com>
2023-05-18 17:43:03 +0200

    not found update btw don't work yet

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -10,7 +10,7 @@ export default function NotFound() {
                 <Image src={'/cat404.png'} alt={'cat404'} width={340} height={340} className={`mb-5 mt-24`}/>
                     <h1 className={`text-[5rem]`}>Meow, so sad...</h1>
                     <p className={`text-[2rem]`}>Nothing here!</p>
-                   <button onClick={()=> router.back()} className={`btn btn-default animate-bounce mt-10`}>Back</button>
+                   <button onClick={()=> router.back()} className={`btn btn-default animate-bounce mt-10 mb-48`}>Back</button>
             </main>
         </>
     );
