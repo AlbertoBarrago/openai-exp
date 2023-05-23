e1bd8f0ae984041c9f80e52353f3a4a267b3aaf5
Alberto Barrago <a.barrago69@gmail.com>
2023-05-23 11:22:02 +0200

    added G analytics

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ import {useRouter} from "next/navigation";
 import {Title} from "@/components/layout/title";
 import {DescriptionFirstPage} from "@/components/home/descriptions/firstPage";
 import {GoToLab} from "@/components/home/goToLab";
+import Script from 'next/script'
 
 
 export default function Home() {
@@ -20,6 +21,21 @@ export default function Home() {
         }
         return (
             <>
+                <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXX"/>
+                <Script
+                    id='google-analytics'
+                    strategy="afterInteractive"
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', 'G-G8986MEGCD', {
+                        page_path: window.location.pathname,
+                        });
+                        `,
+                    }}
+                />
                 <main className={`container mx-auto text-center w-100 p-2`}>
                     <Title title={'OpenAi'} subTitle={'Testing Project'}/>
                     <DescriptionFirstPage router={router}/>
