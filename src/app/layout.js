37ef9d9c1f96c77cdf9d25f2097c2c72328113ab
Alberto Barrago <a.barrago69@gmail.com>
2023-05-23 11:57:58 +0200

    added G analytics, better :D

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,6 +5,7 @@ import {ClerkProvider} from "@clerk/nextjs";
 import Head from "next/head";
 import {Header} from "@/components/layout/header";
 import {Footer} from "@/components/layout/footer";
+import Script from "next/script";
 
 export default function RootLayout({children}) {
     return (
@@ -14,6 +15,21 @@ export default function RootLayout({children}) {
                 <title>Albz - OpenAi</title>
                 <meta name="description" content="OpenAi experiments"/>
             </Head>
+            <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-G8986MEGCD"/>
+            <Script
+                id='google-analytics'
+                strategy="afterInteractive"
+                dangerouslySetInnerHTML={{
+                    __html: `
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', 'G-G8986MEGCD', {
+                        page_path: window.location.pathname,
+                        });
+                        `,
+                }}
+            />
             <body className={`overflow-x-hidden min-h-screen`}>
              <Header/>
                 {children}
--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,6 @@ import {useRouter} from "next/navigation";
 import {Title} from "@/components/layout/title";
 import {DescriptionFirstPage} from "@/components/home/descriptions/firstPage";
 import {GoToLab} from "@/components/home/goToLab";
-import Script from 'next/script'
 
 
 export default function Home() {
@@ -21,21 +20,6 @@ export default function Home() {
         }
         return (
             <>
-                <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXX"/>
-                <Script
-                    id='google-analytics'
-                    strategy="afterInteractive"
-                    dangerouslySetInnerHTML={{
-                        __html: `
-                        window.dataLayer = window.dataLayer || [];
-                        function gtag(){dataLayer.push(arguments);}
-                        gtag('js', new Date());
-                        gtag('config', 'G-G8986MEGCD', {
-                        page_path: window.location.pathname,
-                        });
-                        `,
-                    }}
-                />
                 <main className={`container mx-auto text-center w-100 p-2`}>
                     <Title title={'OpenAi'} subTitle={'Testing Project'}/>
                     <DescriptionFirstPage router={router}/>
